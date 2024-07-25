9e50e7ccceb78a68daff439ce68622a004333e3f
Dan Fooks <danfooks@icloud.com>
2024-07-25 16:29:46 -0400

    configure environment

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'localhost:8000/' // dummy base URL for Customers application
+  },
+});
